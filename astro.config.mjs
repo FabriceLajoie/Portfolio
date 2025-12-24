98185cb419dc2eecdf879f6ecaef77ea631de070
FabriceLajoie <lajoie.fabrice1@gmail.com>
2025-12-24 15:17:03 -0500

    c'est dur la prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://fabricelajoie.github.io',
+    base: isProd ? '/portfolio/' : '/',
 });
